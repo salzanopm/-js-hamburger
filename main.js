78b8cde3e5291dc95e69ac75e699a6865ca7f779
salzanopm <salzanopm@email.com>
2021-11-16 10:12:26 +0100

    correction

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const myButton = document.querySelector('.header-right > a');
  console.log(myButton);
 
 myButton.addEventListener('click',
-function(){
-    const burgerMenu = document.querySelector('.hamburger-menu');
-    burgerMenu.classList.add('active')
-}
+    function(){
+        const burgerMenu = document.querySelector('.hamburger-menu');
+        burgerMenu.classList.add('active')
+    }
 )
 
 const closeButton = document.querySelector('.close');
 closeButton.addEventListener('click',
-function() {
-    const burgerMenu = document.querySelector('.hamburger-menu')
-    burgerMenu.classList.remove('active')
-}
+    function() {
+        const burgerMenu = document.querySelector('.hamburger-menu')
+        burgerMenu.classList.remove('active')
+    }
 )
